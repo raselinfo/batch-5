810bb9aec3591ca8c624c6d27ae984f110e88c9f
raselofficial52 <raselofficial52@gmail.com>
2021-01-25 22:55:21 +0600

    update

---
+++ Class 59 GSAP/js/main.js
@@ -0,0 +1,34 @@
+// var secondBtn = document.getElementById("second");
+
+// function handal() {
+
+//     console.log(counter)
+
+//         if (counter%2==0) {
+//             document.body.style.backgroundColor = "#000";
+//             document.body.style.color = "#fff";
+
+           
+//             console.log(counter)
+
+
+//         }
+//         else {
+//             document.body.style.backgroundColor = "#fff";
+//             bool = true;
+//         }
+//     counter++;
+
+
+
+// }
+// var counter = 2;
+// secondBtn.onclick = handal;
+
+
+
+var spample = document.getElementById("sample");
+
+var text=spample.appendChild("<span>")
+
+text.innerText="Span is here"
