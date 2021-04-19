342fc96a99d21e2763e2f519b50565de74ac30eb
raselinfo <raselinfo52@gmail.com>
2021-04-19 08:43:42 +0600

    update

---
+++ Class 84 (Javascript)/index.js
@@ -0,0 +1,44 @@
+// console.log(5+5)
+// console.log("Rasel Official")
+// console.log("I love JavaScript")
+// console.log(60)
+// console.log(8.0063)
+
+
+// console.log("Sum 5+5 = ", 5 + 5)
+// console.log("Sub 10-5 =", 10 - 5)
+// console.log("Multi 5*5 = ", 5 * 5)
+// console.log("Div 10/5 =", 10 / 5)
+// console.log("Square of 5",5**2)
+// console.log("Square of 6",6*2)
+
+
+// var num1 = 5;
+// var num2 = 5;
+
+
+//console.log(num1)
+//console.log(num2)
+// console.log("Result = ",num1+num2)
+// console.log(num1+num2)
+// console.log(5)
+// console.log("Nahid")
+
+// var firstName="Nahid"
+// var lastName="Rain"
+// var nikeName="Sakib Khan"
+
+// console.log(firstname)
+// console.log("First Name =",firstName,"," ,"Last Name = ", lastName,"," ,"Nike Name = ",nikeName)
+
+
+// var text="I am String"
+// var num=50
+// var string="50"
+
+// var bool=true
+// var boolean=false
+
+// console.log(typeof text)
+// console.log(typeof num)
+// console.log(typeof bool)
---
+++ Class 85 (Javascript)/index.js
@@ -0,0 +1,22 @@
+// var firstName="Rasel";
+// var lastName="Official";
+// var test;
+// console.log(firstName+" 5465 "+lastName)
+// console.log(firstName,lastName)
+// console.log(firstName+"       something           "+lastName)
+
+// console.log("this is browser preview")
+// document.write("<h1>Hi i am<mark> document</mark> <br></h1>")
+// document.write('<h1>'+firstName+" "+lastName+'</h1>')
+// test="value asign"
+// console.log(test="new value asign")
+// alert("Don't press ok")
+// alert(56)
+
+var varName="var"
+const test="const"
+let test2="let"
+document.title="new Learner"
+console.log(varName)
+console.log(test)
+console.log(test2)
