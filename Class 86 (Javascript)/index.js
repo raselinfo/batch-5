44292c0a28bc3f55872f64363141ef8707e03895
raselinfo <raselinfo52@gmail.com>
2021-04-21 16:03:12 +0600

    update

---
+++ Class 86 (Javascript)/index.js
@@ -0,0 +1,102 @@
+// 1.Primitive Data type
+// *Number
+// let int=45;
+// let float=45.534636;
+// console.log(typeof int,typeof float)
+// const num1=Number(45)
+// // const num2=Number("fa;lfjalf")
+// const num3=Number("5654")
+// console.log(typeof num1, num3)
+
+// const num = 79257.6545;
+// const numFloat = Number.parseFloat(num)
+// const numInt = Number.parseInt(num);
+// console.log(numInt)
+
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MIN_SAFE_INTEGER)
+
+// const num1=1;
+// const num2=0;
+// const result=num1/num2;
+// console.log(result)
+// console.log(5*"5")
+// let hex=0xff;
+// let octal=0757;
+// console.log(typeof hex,typeof octal)
+
+
+// *String
+    // "",'',``
+    // const fName="Rasel"
+    // const lName="Official"
+    // const str=""
+    // const bd=`Bangladesh`
+    // console.log(typeof fName,typeof lName)
+    // console.log(typeof str,str)
+    // console.log(typeof bd)
+
+    // let str=String("I love Bangladesh")
+    // let str2=String(80.96)
+    // console.log(typeof str,typeof str2)
+
+
+
+    // *Blooean
+    // let b1=true;
+    // let b2=false;
+    // const b3=0;
+    // const b4=1;
+    // console.log(Boolean(b3), Boolean(b4))
+
+    // console.log(Boolean(" "),Boolean("Rasel"))
+    // console.log(typeof b1, typeof b2)
+    // *Undefine
+//     let num1;
+
+//     // console.log(undefined)
+//     // console.log(null)
+//     console.log(test)
+//     // *Null
+// let test = null;
+// 2.Object Data type
+    // Array
+    // object
+    // function
+
+
+
+// console.log("100"*10)
+// console.log(Number("100")+10)
+// console.log(-"100"+10)
+
+// let num="50"
+// let newNum=parseFloat(num)
+// let newNum=parseInt(num)
+// console.log(typeof newNum)
+
+
+// let num1=50;
+// // let newNum=String(num1)
+// let newNum=num1.toString()
+// console.log(typeof newNum)
+
+// console.log(typeof Infinity.toString())
+// // console.log(`${typeof Infinity}`);
+// console.log(typeof Infinity)
+// console.log(Boolean(-Infinity))
+// console.log(Boolean(2856))
+// console.log(Boolean(0))
+
+// console.log(typeof true.toString())
+// console.log(parseInt(false))
+// console.log(parseInt(true))
+// console.log(parseInt(false))
+// console.log(Number(true))
+// console.log(Number(false))
+
+const str=`js`
+const num=56
+// console.log("i love "+str+" my "fav" number is "+num)
+console.log(`i love ${str} my 'fav' number is ${num}`)
+document.write(`i love ${str} my 'fav' number is ${num}`)
