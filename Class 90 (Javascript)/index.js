63a22c6e5f2c4f32678fc67496038bfde70b881a
raselinfo <raselinfo52@gmail.com>
2021-05-05 16:03:24 +0600

    update

---
+++ Class 90 (Javascript)/index.js
@@ -0,0 +1,38 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+
+// Math
+// console.log(Math.E)
+// console.log(`PI : ${Math.PI}`)
+// console.log(Math.PI)
+// console.log("PI : "+Math.PI)
+// console.log(Math.abs(-50))
+// const negative=-99999;
+// console.log(Math.abs(negative))
+// console.log(Math.floor(444.666))
+// console.log(Math.floor(55.9999))
+// console.log(Math.ceil(3.1))
+// console.log(Math.round(8.56545))
+// console.log(Math.max(0,546000000,897,200,5000))
+// console.log(Math.min(8,9,0,6,-1,-2,-3))
+// console.log(Math.pow(5,4))
+// console.log(Math.sqrt(36))
+// console.log(Math.round(Math.random() * 2 + 1))
+// const numbers=[5,6,8,9,0,55,66,444]
+// const maximumValue = Math.max(...numbers)
+// console.log(`Maximum Value = ${maximumValue}`)
+
+
+// Date
+const date=new Date()
+// console.log(date.toDateString())
+// console.log(date.toTimeString())
+// console.log(date.toLocaleString())
+// console.log(date.toLocaleDateString())
+// console.log(date.toLocaleTimeString())
+// console.log(date.getFullYear())
+// console.log(date.getMonth())
+// console.log(date.getDate())
+// console.log(date.getHours())
+// console.log(date.getMinutes())
+// console.log(date.getSeconds())
+// console.log(date.getMilliseconds())
